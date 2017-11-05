4cb65110f9da6636624107fd7e54da10106c8a58
Harish Hurchurn <Harish@ioctl.co>
2017-11-05 08:35:18 +0000

    Updated id monad test

--- advanced_scala_with_cats/src/test/scala/co/ioctl/chapter4/monads/IdMonad/IdMonadSpec.scala
+++ advanced_scala_with_cats/src/test/scala/co/ioctl/chapter4/monads/IdMonad/IdMonadSpec.scala
@@ -6,20 +6,37 @@ class IdMonadSpec extends FreeSpec with Matchers {
   import co.ioctl.chapter4.Monad.IdMonad.IdMonad
 
   "IdMonad" - {
-    val idMonad: IdMonad[Int] = new IdMonad[Int]{}
-    val x = idMonad.pure(10)
-
     "should provide a pure method which should lift a plain value into the context of the monad" in {
       import cats.Id
-      x shouldBe a [Id[_]]
+      val idMonad: IdMonad[Int] = new IdMonad[Int]{}
+      idMonad.pure(10) shouldBe a [Id[_]]
     }
 
     "should provide a flatMap operation" in {
-      val res = idMonad.flatMap(x)(_ + 1) shouldBe 11
+      val idMonad: IdMonad[Int] = new IdMonad[Int]{}
+      val a = idMonad.pure(1)
+      idMonad.flatMap(a)(_ + 1) shouldBe 2
     }
 
     "should provide a map operation" in {
-      idMonad.map(x)(_ + 1) shouldBe 11
+      val idMonad: IdMonad[Int] = new IdMonad[Int]{}
+      val b = idMonad.pure(1)
+      idMonad.map(b)(_ + 1) shouldBe 2
+    }
+
+    "should allow for-comprehensions to work" in {
+      import cats.implicits._
+
+      val idMonad: IdMonad[Int] = new IdMonad[Int]{}
+      val a = idMonad.pure(3)
+      val b = idMonad.flatMap(a)(_ + 1)
+
+      val res = for {
+        x <- a
+        y <- b
+      } yield x + y
+
+      res shouldBe 7
     }
   }
 }
